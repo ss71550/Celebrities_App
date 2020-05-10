b45f3af8999925b63c26d87176a5895644291ddc
ss71550 <sophiahstewart@gmail.com>
2020-05-10 16:04:57 -0500

    changes popUps to class, hide now working

--- Celebrities.js
+++ Celebrities.js
@@ -1,17 +1,26 @@
 $(document).ready(() => {
-  let $popUps= $("#popUps");
+  let $popUps= $(".popUps");
   $popUps.hide();
 //javascript only fetching step one for id, when should fetch all divs
 
-  let $welcome = $(".welcome");
+  let $welcome = $("#welcome");
   let $getStartedButton = $("#getStarted");
 
-  let $step1 = $(".step1");
+  let $step1 = $("#step1");
   let $addCard= $("#add_card");
   let $finishedCards = $("#finishedCards");
 
-  let $step2 = $(".step2");
+  let $step2 = $("#step2");
   let $beginGame = $("#beginGame");
+
+  let $round1 = $("#round1");
+  let $start1 = $("#start1");
+
+  let $round2 = $("#round2");
+  let $start2 = $("#start2");
+
+  let $round3 = $("#round3");
+  let $start3 = $("#start3");
 
 
 $getStartedButton.on({
@@ -21,11 +30,11 @@ $getStartedButton.on({
     }
 });
 
-let $round1= [];//round 1 card array
+let $round1Cards= [];//round 1 card array
 $addCard.on({
     click: () => {
     let celebrity_card = $("#card_input").val();
-    $round1.push("celebrity_card");
+    $round1Cards.push("celebrity_card");
     }
 });
 
@@ -45,4 +54,22 @@ $beginGame.on({
     }
 });
 
+ //the following hide round directions= gameboard beneath
+ $start1.on({
+    click: () => {
+      $round1.hide();
+    }
+});
+
+  $start2.on({
+    click: () => {
+      $round2.hide();
+    }
+});
+
+  $start3.on({
+    click: () => {
+      $round3.hide();
+    }
+});
 })
